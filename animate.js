f63ae82252f3ad8d722335f55718deaa71a05444
kevinqian96 <kevinqian@Kevins-MacBook-Air.local>
2016-04-17 03:06:38 -0700

    brand

--- animate.js
+++ animate.js
@@ -1,11 +1,12 @@
 var heart = false;
-$('.lv').click(function(){
-  if(!heart){
-    $(this).addClass('fa-heart').removeClass('fa-heart-o');
-    heart = true;
-  }
-  else {
-    $(this).removeClass('fa-heart').addClass('fa-heart-o');
-    heart = false;
-  }
-}); 
+$('.lv').click(function() {
+	if (!heart) {
+		$(this).addClass('fa-heart').removeClass('fa-heart-o');
+		heart = true;
+	}
+
+  if (heart){
+		$(this).removeClass('fa-heart').addClass('fa-heart-o');
+		heart = false;
+	}
+};

67cd948c671e7180d83f55624cedaf09a3a919f5
kevinqian96 <kevinqian@Kevins-MacBook-Air.local>
2016-04-17 06:42:42 -0700

    i suck

--- animate.js
+++ animate.js
@@ -1,10 +1,5 @@
 var heart = false;
 $('.lv').click(function() {
-	if (!heart) {
-		$(this).addClass('fa-heart').removeClass('fa-heart-o');
-		heart = true;
-	}
-  else {
-		$(this).removeClass('fa-heart').addClass('fa-heart-o');
-		heart = false;
-	});
+  $('fa-heart-o').hide();
+  $('fa-heart').show();
+});
